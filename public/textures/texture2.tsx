511704771671ae341e0c332b69109fdfb1e75387
Valentin Giorgetti <giorgettivalentin@gmail.com>
2019-12-15 23:08:07 +0100

    finished all maps

---
+++ public/textures/texture2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="texture2" tilewidth="16" tileheight="16" tilecount="660" columns="33">
+ <image source="../../../../../../Downloads/gfx/gfx/objects.png" width="528" height="320"/>
+</tileset>
